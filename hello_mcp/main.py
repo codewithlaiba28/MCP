26aa668dd397e4f622e0b802105a5a5b263bb37f
Laiba Khan <codewithlaiba28@gmail.com>
2025-10-14 18:59:17 +0500

    add

--- hello_mcp/main.py
+++
@@ -1,9 +0,0 @@
-from mcp.server.fastmcp import FastMCP
-
-mcp= FastMCP(name="mcp", stateless_http=True)
-
-@mcp.tool(name="search_online", description="Search online for a query")
-def online_search(query: str):
-    return f"Hi, How are you . Your answer is {query}"
-
-mcp_run = mcp.streamable_http_app()
