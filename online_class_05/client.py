e49409a13079dce974044d6bc417d9cf87890a91
Laiba Khan <codewithlaiba28@gmail.com>
2025-10-23 13:31:50 +0500

    add

--- online_class_05/client.py
+++ online_class_05/client.py
@@ -46,6 +46,13 @@ class MCPClient:
         result:types.ListResourcesResult= await self._sess.list_resources()
         return result.resources
     
+
+    async def list_resources_Template(self) ->list[types.Resource]:
+        assert self._sess, "session not available"
+        result: types.ListResourcesResult= await self._sess.list_resource_templates()
+        # print(f"LIST RESOURCES TEMPLATE DICT{result.__dict__}")
+        return result.resourceTemplates
+
     async def read_resources(self, uri:str) ->types.ReadResourceResult:
         assert self._sess, "session not available"
         _url = AnyUrl(uri)
@@ -66,15 +73,29 @@ async def main():
         # tools = await client.list_tools()
         # print(tools, "tools")
 
-        resources = await client.list_resources()
-        data= await client.read_resources("docs://documents")
+        # resources = await client.list_resources()
+        # data= await client.read_resources("docs://documents")
 
-        print(resources[0].uri, "first resource uri")
-        print(data, "data from first resource")
+        # print(resources[0].uri, "first resource uri")
+        # print(data, "data from first resource")
          
         # for r in resources:
         #     data= await client.read_resources(r.uri)
         #     print(f"Data for {r.uri}: ___________________{data}")
 
+        # template = await client.list_resources_Template()
+        # print(template, "template resources")
+        # for i in template:
+        #    print("__________________________________________________________________________")
+        #    print(f"Template Resource: {i.uriTemplate}") 
+        #    data = await client.read_resources(i.uriTemplate)
+        #    print("__________________________________________________________________________")
+        template = await client.list_resources_Template()
+        intro_uri = template[0].uriTemplate.replace("{doc_name}", "intro")
+        data = await client.read_resources(intro_uri)
+        print("Indtro Document Data:",data)
+        # print(f"Resources Template: {template[0].uriTemplate}")
+        # template = await client.list_resources_Template()
+        # print("Resource Templates:", template[0].uriTemplate)
 asyncio.run(main())
 
--- online_class_05/server.py
+++ online_class_05/server.py
@@ -14,6 +14,20 @@ def list_docs():
     """list available document names"""
     return list(docs.keys())
 
-print(f"list_docs: {list_docs()}")
+@mcp.resource("docs://documents/{doc_name}", 
+              mime_type="text/plain")
+
+def read_docs(doc_name:str):
+    """Read a specific document."""
+    if doc_name in docs:
+        return docs[doc_name]
+    else:
+        raise mcp.ResourceNotFound(f"Document '{doc_name}' not found.")
+
+# def read_docs(doc_name:str):
+#     """read document by name"""
+#     return docs.get(doc_name, "Document not found.")
+
+# print(f"list_docs: {list_docs()}")
 
 mcp_server = mcp.streamable_http_app()
