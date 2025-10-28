f41caab67a58a9415d4cbdf41aa645c1f041b3cc
Laiba Khan <codewithlaiba28@gmail.com>
2025-10-28 16:27:31 +0500

    update

--- online_class_06/class_code/main.py
+++ online_class_06/class_code/main.py
@@ -4,7 +4,8 @@ from mcp.server.fastmcp.prompts import base
 mcp = FastMCP(stateless_http=True, name="Hello prompt")
 
 @mcp.prompt()
-async def hello_prompt(name: str) -> str:
-    return f"Hello, {name}!" 
+async def hello_prompt() -> list[base.UserMessage]:
+    user_message = f"Hello, From Agentic Mars!"
+    return {base.UserMessage(content=user_message)} , {base.AssistantMessage(content="Hi there! How can I assist you today?")}
 
 mcp_app = mcp.streamable_http_app() 
