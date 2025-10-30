2ce14dda3cde38e92be5d674a9f78629d950c7b8
Laiba Khan <codewithlaiba28@gmail.com>
2025-10-30 15:04:24 +0500

    add

--- online_class_7/hello.py
+++ online_class_7/hello.py
@@ -1,4 +1,21 @@
-name = input("What is your name?")
+# name = input("What is your name?")
 
-print(f"Hello, {name}!")
-print("Welcome to the program.")
+# print(f"Hello, {name}!")
+# print("Welcome to the program.")
+
+
+# while True:
+#     age = input("What is your age? ")
+#     print(f"Hello, {name}! you are {age} years old.")
+
+#     if age.lower() == "exit":
+#         print("Exiting the program. Goodbye!")
+#         break
+
+
+def hello(data):
+    return data
+
+print(hello("Hello world!\n"))
+
+print(hello("This is a test message.")) 
